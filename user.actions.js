0c9e9fbc8706584d96b9371b010e049d57da150c
HodayaGitHub <110330009+HodayaGitHub@users.noreply.github.com>
2024-02-01 18:44:40 +0200

    Connect user order to db

---
+++ user.actions.js
@@ -0,0 +1,103 @@
+import { userService } from "../../services/user.service.js"
+import { socketService } from "../../services/socket.service.js"
+import { store } from '../store.js'
+
+import { showErrorMsg } from '../../services/event-bus.service.js'
+import { LOADING_DONE, LOADING_START } from "../reducers/system.reducer.js"
+import { UPDATE_USER, REMOVE_USER, SET_USER, SET_USERS, SET_WATCHED_USER } from "../reducers/user.reducer.js"
+
+export async function loadUsers() {
+    try {
+        store.dispatch({ type: LOADING_START })
+        const users = await userService.getUsers()
+        store.dispatch({ type: SET_USERS, users })
+    } catch (err) {
+        console.log('UserActions: err in loadUsers', err)
+    } finally {
+        store.dispatch({ type: LOADING_DONE })
+    }
+}
+
+
+export async function removeUser(userId) {
+    try {
+        await userService.remove(userId)
+        store.dispatch({ type: REMOVE_USER, userId })
+    } catch (err) {
+        console.log('UserActions: err in removeUser', err)
+    }
+}
+
+export async function login(credentials) {
+    try {
+        const user = await userService.login(credentials)
+        store.dispatch({
+            type: SET_USER,
+            loggedInUser: user
+        })
+        socketService.login(user)
+        return user
+    } catch (err) {
+        console.log('Cannot login', err)
+        throw err
+    }
+}
+
+export async function signup(credentials) {
+    try {
+        const user = await userService.signup(credentials)
+        store.dispatch({
+            type: SET_USER,
+            loggedInUser: user
+        })
+        socketService.login(user)
+        return user
+    } catch (err) {
+        console.log('Cannot signup', err)
+        throw err
+    }
+}
+
+export async function logout() {
+    try {
+        await userService.logout()
+        store.dispatch({
+            type: SET_USER,
+            loggedInUser: null
+        })
+        userService.removeLoacalUserFromSession()
+        socketService.logout()
+    } catch (err) {
+        console.log('Cannot logout', err)
+        throw err
+    }
+}
+
+export async function loadUser(userId) {
+    try {
+        const user = await userService.getById(userId)
+        return user
+    } catch (err) {
+        showErrorMsg('Cannot load user')
+        console.log('Cannot load user', err)
+    }
+}
+
+export function getActionUpdateUser(user) {
+    return {
+        type: UPDATE_USER,
+        user
+    }
+}
+
+export async function updateUser(user) {
+    try {
+        const savedUser = await userService.save(user)
+        console.log('Updated user:', savedUser)
+        // store.dispatch(getActionUpdateUser(savedUser))
+        return savedUser
+    } catch (err) {
+        console.log('Cannot save user', err)
+        throw err
+    }
+}
